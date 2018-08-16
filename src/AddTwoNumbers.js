ec818253d1735e2b52b4e1a654949b58500ee2de
tangdaijia <253141924@qq.com>
2018-08-17 02:14:08 +0800

    AddTwoNumbers

--- src/AddTwoNumbers.js
+++ src/AddTwoNumbers.js
@@ -34,8 +34,8 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-//第一次的思路为转换成数字然后进行相加 然后再重新转回来
-//遇见例子：
+// 第一次的思路为转换成数字然后进行相加 然后再重新转回来
+// 遇见例子：
 // 输入：
 // [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
 // [5,6,4]
@@ -71,4 +71,72 @@
 //     }
 //     return dx;
 // }
+
+
+// 忘记考虑直接进位的问题
+// 输入：
+// [5]
+// [5]
+// 输出：
+// [0]
+// 预期：
+// [0,1]
+
+// 未考虑只有但单链表时大于9的操作,然后有重复加入数组的情况
+// 输入：
+// [1]
+//     [9,9]
+// 输出：
+// [0,10]
+// 预期：
+// [0,0,1]
+
+var addTwoNumbers = function(l1, l2) {
+    var temp1 = l1;
+    var temp2 = l2;
+    var result =[];
+
+    while(temp1||temp2){
+        if(temp1&&temp2){
+            var sum;
+            if((temp1.val+temp2.val)>9)
+                sum = temp1.val+temp2.val-10;
+            else sum = temp1.val+temp2.val;
+            result.push(sum);
+
+            if((temp1.val+temp2.val)>9){
+                if(temp1.next)
+                    temp1.next.val++;
+                else if(temp2.next)
+                    temp2.next.val++;
+                else {
+                    result.push(1);
+                }
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }else if(temp1){
+            if(temp1.val>9){
+                result.push(temp1.val-10);
+                if(temp1.next)
+                    temp1.next.val++;
+                else result.push(1);
+            }else {
+                result.push(temp1.val);
+            }
+            temp1 = temp1.next;
+        }else if(temp2){
+            if(temp2.val>9){
+                result.push(temp2.val-10);
+                if(temp2.next)
+                    temp2.next.val++;
+                else result.push(1);
+            }else {
+                result.push(temp2.val);
+            }
+            temp2 = temp2.next;
+        }
+    }
+    return result;
+};
 
