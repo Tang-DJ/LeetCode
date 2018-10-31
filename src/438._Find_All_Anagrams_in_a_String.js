818b80e0e1ed59b5bf083de3b0a338cda74ccf77
tangdaijia <253141924@qq.com>
2018-10-31 23:52:48 +0800

    438 too slow

--- src/438._Find_All_Anagrams_in_a_String.js
+++ src/438._Find_All_Anagrams_in_a_String.js
@@ -29,34 +29,38 @@
 //     The substring with start index = 1 is "ba", which is an anagram of "ab".
 //     The substring with start index = 2 is "ab", which is an anagram of "ab".
 
+
+
 /**
  * @param {string} s
  * @param {string} p
  * @return {number[]}
+ * too slow
+ * Runtime: 9424 ms, faster than 0.96% of JavaScript online submissions for Find All Anagrams in a String.
  */
-var findAnagrams = function(s, p) {
-    var arr=s.split('');
-    var target = p.split('');
-    var left=0;
-    target.sort();
-    var result = [];
-    while(left+target.length-1<arr.length){
-        var temp = arr.slice(left,left+target.length);
-        temp.sort();
-        for(var i =0;i<target.length;i++){
-            if(target[i]!==temp[i]){
-                if(i) left+=i;
-                else left++;
-                break;
-            }
-        }
-        if(i===target.length){
-            result.push(left);
-            left++;
-        }
-    }
-    return result;
-};
-
-var re = findAnagrams("abacbabc","abc");
-console.log(re);
+// var findAnagrams = function(s, p) {
+//     var arr=s.split('');
+//     var target = p.split('');
+//     var left=0;
+//     target.sort();
+//     var result = [];
+//     while(left+target.length-1<arr.length){
+//         var temp = arr.slice(left,left+target.length);
+//         temp.sort();
+//         for(var i =0;i<target.length;i++){
+//             if(target[i]!==temp[i]){
+//                 if(i) left+=1;
+//                 else left++;
+//                 break;
+//             }
+//         }
+//         if(i===target.length){
+//             result.push(left);
+//             left++;
+//         }
+//     }
+//     return result;
+// };
+//
+// var re = findAnagrams("cbaebabacd","abc");
+// console.log(re);
